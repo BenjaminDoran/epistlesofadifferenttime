c049c400dd7f825fb9e72fda7e98f29bff52a7d9
BenjaminDoran <20494558+BenjaminDoran@users.noreply.github.com>
2021-09-04 04:20:48 -0500

    rename to latest

--- docs/versions.js
+++
@@ -1,6 +0,0 @@
-var PUBLISH_ROOT = '/epistlesofadifferenttime/docs';
-var PUBLISH_VERSION = "docs";
-var PUBLISH_VERSIONS = [
-    ["docs","/epistlesofadifferenttime/docs/index.html"],
-    ["0.1.0","/epistlesofadifferenttime/0.1.0/index.html"],
-];
